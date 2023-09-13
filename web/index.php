f4f9ce6172301f1bb04fc5b20ead4d1dd7f046da
Tangui Morlier <tangui@tangui.eu.org>
2023-09-13 17:04:50 +0200

    Bouton plus de stats

--- web/index.php
+++ web/index.php
@@ -4,28 +4,38 @@
       <div class="col-12">
         <div class="card mb-4 rounded-3 shadow-sm">
           <div class="card-header py-3">
-            <h4 class="my-0 fw-normal">Les adhésions</h4>
+            <h4 class="my-0 fw-normal">Les adhésions <span class="text-muted">quotidiennes</span></h4>
           </div>
           <div class="card-body">
               <?php include('generated/adhesions_01_adhesion_des_derniers_jours.html'); ?>
               <p>Le nombre d'adhésions quotidiennes sur les 30 derniers jours</p>
+              <p class="text-muted">Données issues de zelty</p>
           </div>
-          <div class="card-footer text-body-secondary">
-              <a href="adhesions.php">Plus de stats sur les adhésions</a>
+          <div class="card-footer text-body-primary">
+              <div class="px-5 float-end">
+                  <a href="adhesions.php" class="btn btn-primary">Voir plus de stats sur les adhésions &gt;&gt;</a>
+              </div>
           </div>
         </div>
       </div>
+
+      <div class="card-footer text-body-secondary">
+      </div>
+
       <div class="col-12">
         <div class="card mb-4 rounded-3 shadow-sm">
           <div class="card-header py-3">
-            <h4 class="my-0 fw-normal">Les évènements</h4>
+            <h4 class="my-0 fw-normal">Les évènements <span class="text-muted">hebdomadaires</span></h4>
           </div>
           <div class="card-body">
               <?php include('generated/evenements_01_events_par_semaine.html'); ?>
               <p>Le nombre d'évènements par semaine</p>
+              <p class="text-muted">Données issues du site du <a href="https://www.lebarcommun.fr/events/">bar commun</a></p>
           </div>
           <div class="card-footer text-body-secondary">
-              <a href="evenements.php">Plus de stats sur les évènements</a>
+              <div class="px-5 float-end">
+              <a href="evenements.php" class="btn btn-primary">Voir plus de stats sur les évènements &gt;&gt;</a>
+              </div>
           </div>
         </div>
       </div>
@@ -33,14 +43,17 @@
       <div class="col-12">
         <div class="card mb-4 rounded-3 shadow-sm">
           <div class="card-header py-3">
-            <h4 class="my-0 fw-normal">Les services</h4>
+            <h4 class="my-0 fw-normal">Les services <span class="text-muted">mensuels</span></h4>
           </div>
           <div class="card-body">
               <?php include('generated/baristas_05_nb_services_réalisés_par_mois.html'); ?>
-              <p>Le nombre de services par mois</p>
+              <p>Le nombre de services réalisés par mois. (Pour un créneau, 1 référent et 2 baristas = 3 services réalisés)</p>
+              <p class="text-muted">Données issues du calendrier partagé des services</p>
           </div>
           <div class="card-footer text-body-secondary">
-              <a href="services.php">Plus de stats sur les services</a>
+              <div class="px-5 float-end">
+              <a href="services.php" class="btn btn-primary">Voir plus de stats sur les services &gt;&gt;</a>
+              </div>
           </div>
         </div>
       </div>
@@ -48,14 +61,17 @@
       <div class="col-12">
         <div class="card mb-4 rounded-3 shadow-sm">
           <div class="card-header py-3">
-            <h4 class="my-0 fw-normal">Les Recettes</h4>
+            <h4 class="my-0 fw-normal">Les Recettes <span class="text-muted">annuelles</span></h4>
           </div>
           <div class="card-body">
               <?php include('generated/recettes_03_ca_total_par_an.html'); ?>
-              <p>Les recettes annuelles</p>
+              <p>Les recettes annuelles et recette "à date" en euro</p>
+              <p class="text-muted">Données issues de Zelty</p>
           </div>
           <div class="card-footer text-body-secondary">
-              <a href="recettes.php">Plus de stats sur les recettes</a>
+              <div class="px-5 float-end">
+              <a href="recettes.php" class="btn btn-primary">Voir plus de stats sur les recettes &gt;&gt;</a>
+              </div>
           </div>
         </div>
       </div>
