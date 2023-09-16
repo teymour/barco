f6b2981050fa25e62be3b34c4d946df17ef8e163
Tangui Morlier <tangui@tangui.eu.org>
2023-09-16 16:23:40 +0200

    Conservation du nom des produits dans le temps

--- bin/zelty2csv.py
+++ bin/zelty2csv.py
@@ -12,9 +12,9 @@ with open(zeltyfile) as f:
     for o in zeltydata['orders']:
         for i in o['items']:
             date = datetime.fromisoformat(o['closed_at']) - timedelta(hours=4)
-            converted.append([date.strftime('%Y-%m-%d'), o['uuid'], i['name'].replace(';', ' ').replace(',', ' '), i['price']['final_amount_inc_tax'] / 100])
+            converted.append([date.strftime('%Y-%m-%d'), o['uuid'], i['item_id'], i['name'].replace(';', ' ').replace(',', ' '), i['price']['final_amount_inc_tax'] / 100])
 
-z = pd.DataFrame(converted, columns=['date', 'order uuid', 'produit', 'price'])
+z = pd.DataFrame(converted, columns=['date', 'order uuid', 'pid', 'produit', 'price'])
 p2t = pd.read_csv("produit2type.csv", sep=';').set_index('produit')['type'].to_dict()
 z['type'] = z['produit'].map(p2t)
 print(z.to_csv(index=False, sep=';', decimal=','))
