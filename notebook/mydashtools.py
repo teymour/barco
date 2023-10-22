f6d5dd77cf66249be4942c652ae7d63f93efff50
Tangui Morlier <tangui@tangui.eu.org>
2023-10-22 10:19:17 +0200

    Ajout des activités dans les graphs quotidiens

--- notebook/mydashtools.py
+++ notebook/mydashtools.py
@@ -16,3 +16,11 @@ def fig_save_or_show(fig, file):
         fig.show()
     else:
         fig.write_html(os.path.dirname(os.path.realpath(__file__))+"/../web/generated/"+file+".html",include_plotlyjs=False)
+
+def true_or_html_to_file(table, file):
+    if is_notebook():
+        return True
+    else:
+        with open(os.path.dirname(os.path.realpath(__file__))+"/../web/generated/"+file+".html", 'w') as fo:
+           fo.write(table.to_html())
+        return False
