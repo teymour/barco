77ff4520253667b1ca1f8aade59dc4b9d0aaa5e2
Tangui Morlier <tangui@tangui.eu.org>
2024-04-10 23:27:28 +0200

    Permet de générer les données pour une année donnée

--- notebook/mydashtools.py
+++ notebook/mydashtools.py
@@ -10,17 +10,21 @@ def is_notebook() -> bool:
     except NameError:
         return False      # Probably standard Python interpreter
 
-def fig_save_or_show(fig, file):
+def fig_save_or_show(fig, file, year = ''):
     fig.update_layout(margin=dict(l=20, r=20, t=20, b=20))
     if is_notebook():
         fig.show()
     else:
-        fig.write_html(os.path.dirname(os.path.realpath(__file__))+"/../web/generated/"+file+".html",include_plotlyjs=False)
+        if year:
+            year += '_'
+        fig.write_html(os.path.dirname(os.path.realpath(__file__))+"/../web/generated/%s%s.html" % (year, file),include_plotlyjs=False)
 
-def true_or_html_to_file(table, file):
+def true_or_html_to_file(table, file, year = ''):
     if is_notebook():
         return True
     else:
-        with open(os.path.dirname(os.path.realpath(__file__))+"/../web/generated/"+file+".html", 'w') as fo:
+        if year:
+            year += '_'
+        with open(os.path.dirname(os.path.realpath(__file__))+"/../web/generated/%s%s.html" % (year, file), 'w') as fo:
            fo.write(table.to_html())
         return False
